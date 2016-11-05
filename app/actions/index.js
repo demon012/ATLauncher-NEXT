c9cfa4b7fefdbeeb62dcb985545b9b5b2ff80fea
Alan James Jenkins <alan.james.jenkins@gmail.com>
2016-11-05 23:34:29 +0000

    Start on receiving news items.

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,7 +1,21 @@
+import fetch from 'isomorphic-fetch'
+
 export const TEST = 'TEST';
 
 export function increment() {
     return {
         type: TEST
     };
+}
+
+function receiveNews(json) {
+}
+
+export function fetchNews() {
+    return dispatch => {
+        dispatch(requestNews())
+        return fetch('https://download.nodecdn.net/containers/atl/launcher/json/news.json')
+        .then(response => response.json())
+        .then(json => dispatch(receiveNews(json)))
+    }
 }
